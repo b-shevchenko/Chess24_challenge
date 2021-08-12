827963b4f19cb5f8d69f4a9ab1640ddc2a23a2e2
Bohdan <bogdan.shevchenko@voypost.com>
2021-08-12 22:41:36 +0300

    button disabling

--- components/Button/styles.ts
+++ components/Button/styles.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
+import { Props } from './types'
 
-export const StyledButton = styled.button`
-  background-color: ${({ theme }) => theme.colors.black[0]};
+export const StyledButton = styled.button<Props>`
+  background-color: ${(props) => props.disabled ? props.theme.colors.white[0] : props.theme.colors.black[0]};
   border-radius:${({ theme }) => `${theme.radii[4]}px`};
   border:${({ theme }) => `${theme.borderWidths[3]}px solid ${theme.colors.gray[0]}`};
   display:inline-block;
   cursor:pointer;
-  color: ${({ theme }) => theme.colors.white[0]};
+  color: ${(props) => props.disabled ? props.theme.colors.black[0] : props.theme.colors.white[0]};
   font-size: ${({ theme }) => `${theme.fontSizes[4]}px`};
   font-weight:bold;
   padding: ${({ theme }) => `${theme.spacing[2]}px ${theme.spacing[5]}px`};
