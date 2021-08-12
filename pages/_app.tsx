9542a72e04359955f5fdbe7787cc10054964d91b
Bohdan <bogdan.shevchenko@voypost.com>
2021-08-12 17:56:30 +0300

    fetch data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Props } from './types';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, data }: Props) {
+  return <Component {...pageProps} data={data} />
 }
-export default MyApp
+
+MyApp.getInitialProps = async ({ Component, ctx }: any) => {
+  const response = await fetch('https://baconipsum.com/api/?type=meat-and-filler');
+  const data = await response.json();
+
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  return { pageProps, data };
+};
+export default MyApp;
