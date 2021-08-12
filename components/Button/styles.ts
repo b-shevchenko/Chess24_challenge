7da986fbb27911d0f54423842ede021269791a8e
Bohdan <bogdan.shevchenko@voypost.com>
2021-08-12 17:54:38 +0300

    Styled button

---
+++ components/Button/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+  background-color:#000000;
+  border-radius:32px;
+  border:5px solid #dcdcdc;
+  display:inline-block;
+  cursor:pointer;
+  color:#ffffff;
+  font-size:24px;
+  font-weight:bold;
+  padding:6px 24px;
+  fill: white;
+`
